ee7b16cdfa3840d7597e68f626786e85a5e9e95c
Geon Ha Lee <neoera1346@naver.com>
2021-01-26 16:01:42 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import express from 'express';
+
+const app = express();
 
 ReactDOM.render(
   <React.StrictMode>
